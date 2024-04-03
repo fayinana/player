72c00e89dd497c881e945230a27570043e2a9a31
edekefayinana <151903622+fayinana@users.noreply.github.com>
2024-04-03 11:18:28 +0300

    Create data.js
    the file that is locate the path of the song

---
+++ data.js
@@ -0,0 +1,109 @@
+let songs = [
+
+{
+    name:'tsione',
+    path:'G YOHANES (1).mp3',
+    artist:'g/yohans',
+    cover:'IMG_20220618_015744_419.jpg'
+
+},
+{
+    name:'a',
+    path:'G YOHANES (2).mp3',
+    artist:'g/yohans',
+    cover:'IMG_20220618_012659_345.jpg'
+
+},
+{
+    name:'b',
+    path:'G YOHANES (3).mp3',
+    artist:'g/yohans',
+    cover:'IMG_20220618_015744_419.jpg'
+
+},
+{
+    name:'natanem',
+    path:'G YOHANES (4).mp3',
+    artist:'g/yohans',
+    cover:'IMG_20220618_012659_345.jpg'
+
+},
+{
+    name:'c',
+    path:'G YOHANES (5).mp3',
+    artist:'g/yohans',
+    cover:'IMG_20220618_012659_345.jpg'
+
+},
+{
+    name:'d',
+    path:'G YOHANES (6).mp3',
+    artist:'g/yohans',
+    cover:'IMG_20220618_015744_419.jpg'
+
+},
+{
+    name:'e',
+    path:'G YOHANES (7).mp3',
+    artist:'g/yohans',
+    cover:'IMG_20220618_015744_419.jpg'
+
+},
+{
+    name:'f',
+    path:'G YOHANES (8).mp3',
+    artist:'g/yohans',
+    cover:'IMG_20220618_015744_419.jpg'
+
+},
+{
+    name:'g',
+    path:'G YOHANES (9).mp3',
+    artist:'g/yohans',
+    cover:'IMG_20220618_015744_419.jpg'
+
+},
+{
+    name:'h',
+    path:'G YOHANES (10).mp3',
+    artist:'g/yohans',
+    cover:'IMG_20220618_015744_419.jpg'
+
+},
+{
+    name:'i',
+    path:'G YOHANES (11).mp3',
+    artist:'g/yohans',
+    cover:'IMG_20220618_015744_419.jpg'
+
+},
+{
+    name:'j',
+    path:'G YOHANES (12).mp3',
+    artist:'g/yohans',
+    cover:'IMG_20220618_015744_419.jpg'
+
+},
+
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
