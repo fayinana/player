f2ab97d4e640a646cb59f04e0ab2282005465cfd
edekefayinana <151903622+fayinana@users.noreply.github.com>
2024-04-03 09:09:13 +0000

    this comite is fron vs code

--- data.js
+++ data.js
@@ -1,52 +1,110 @@
 let songs = [
 
-{
-    name:'tsione',
-    path:'song/G YOHANES (1).mp3',
-    artist:'g/yohans',
-    cover:'song/IMG_20220618_015744_419.jpg'
-
-},
-{
-    name:'natanem',
-    path:'song/G YOHANES (4).mp3',
-    artist:'g/yohans',
-    cover:'song/IMG_20220618_012659_345.jpg'
-
-},
-{
-    name:'c',
-    path:'song/G YOHANES (5).mp3',
-    artist:'g/yohans',
-    cover:'song/IMG_20220618_012659_345.jpg'
-
-},
-{
-    name:'d',
-    path:'song/G YOHANES (6).mp3',
-    artist:'g/yohans',
-    cover:'song/IMG_20220618_015744_419.jpg'
-
-},
-]
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    {
+        name:'tsione',
+        path:'./song/G YOHANES (1).mp3',
+        artist:'g/yohans',
+        cover:'./song/IMG_20220618_015744_419.jpg'
+    
+    },
+    {
+        name:'a',
+        path:'./song/G YOHANES (2).mp3',
+        artist:'g/yohans',
+        cover:'./song/IMG_20220618_012659_345.jpg'
+    
+    },
+    {
+        name:'b',
+        path:'./song/G YOHANES (3).mp3',
+        artist:'g/yohans',
+        cover:'./song/IMG_20220618_015744_419.jpg'
+    
+    },
+    {
+        name:'natanem',
+        path:'./song/G YOHANES (4).mp3',
+        artist:'g/yohans',
+        cover:'./song/IMG_20220618_012659_345.jpg'
+    
+    },
+    {
+        name:'c',
+        path:'./song/G YOHANES (5).mp3',
+        artist:'g/yohans',
+        cover:'./song/IMG_20220618_012659_345.jpg'
+    
+    },
+    {
+        name:'d',
+        path:'./song/G YOHANES (6).mp3',
+        artist:'g/yohans',
+        cover:'./song/IMG_20220618_015744_419.jpg'
+    
+    },
+    {
+        name:'e',
+        path:'./song/G YOHANES (7).mp3',
+        artist:'g/yohans',
+        cover:'./song/IMG_20220618_015744_419.jpg'
+    
+    },
+    {
+        name:'f',
+        path:'./song/G YOHANES (8).mp3',
+        artist:'g/yohans',
+        cover:'./song/IMG_20220618_015744_419.jpg'
+    
+    },
+    {
+        name:'g',
+        path:'./song/G YOHANES (9).mp3',
+        artist:'g/yohans',
+        cover:'./song/IMG_20220618_015744_419.jpg'
+    
+    },
+    {
+        name:'h',
+        path:'./song/G YOHANES (10).mp3',
+        artist:'g/yohans',
+        cover:'./song/IMG_20220618_015744_419.jpg'
+    
+    },
+    {
+        name:'i',
+        path:'./song/G YOHANES (11).mp3',
+        artist:'g/yohans',
+        cover:'./song/IMG_20220618_015744_419.jpg'
+    
+    },
+    {
+        name:'j',
+        path:'./song/G YOHANES (12).mp3',
+        artist:'g/yohans',
+        cover:'./song/IMG_20220618_015744_419.jpg'
+    
+    },
+    
+    ]
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
